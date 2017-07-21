22044515a27f35fb972aab01543313ad2b2b1e48
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-07-21 17:50:17 -0300

    working with sockets

--- server/server.js
+++ server/server.js
@@ -18,9 +18,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connect');
 
+    socket.emit('newEmail',  {
+        from : 'mike@example.com',
+        text : 'Hey. What is going on',
+        createAt: 312312321321
+    });
+
+    socket.emit('newMessage',  {
+        from : 'mike@example.com',
+        text : 'I dont care',
+        createAt: 312312321321
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail');
+        console.log(newEmail);
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage');
+        console.log(newMessage);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
+
+    
 });
 
 server.listen(port, () => {
