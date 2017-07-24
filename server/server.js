6a2a928dbb4e1360d4964382e4cc258a5e93bee1
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-07-24 13:30:22 -0300

    Emit new message on create mesage

--- server/server.js
+++ server/server.js
@@ -18,14 +18,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connect');
 
-    socket.emit('newMessage',  {
-        from : 'mike@example.com',
-        text : 'I dont care',
-        createAt: 312312321321
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from : message.from,
+            message : message.text,
+            createAt : new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
